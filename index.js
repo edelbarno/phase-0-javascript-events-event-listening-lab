457f9452a152adec4bad11ae4625dab8a0cc2230
edelbarno <barnoedel12@gmail.com>
2024-04-05 23:21:38 +0300

    add events lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const input = document.querySelector('button');
+
 function addingEventListener() {
+    document.getElementById('button');
 }
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  input.addEventListener('click',clickAlert)
+
+  const button = getElementById('button');
+  button.addEventListener('click', clickAlert)
